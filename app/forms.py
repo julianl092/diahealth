7b54f300e80ab8fa434bbc1a467d6b5da85eba27
Julian Lee <julian.lee@thecrimson.com>
2020-01-10 23:20:07 -0500

    Toggle Login

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+class SignupForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,35 @@
 from flask_login import current_user, login_user
 from app.models import User
-from app.forms import LoginForm
+from app.forms import LoginForm, SignupForm
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import logout_user
 
+
+@app.route('/')
+def index(): 
+    return render_template('base.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    loginform = LoginForm()
+    signupform = SignupForm()
+    if request.method == 'GET':
+        return render_template('login.html', title='Sign In', loginform=loginform, signupform=signupform)
+    if request.method == 'POST':
+        if loginform.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid username or password')
+                return redirect(url_for('login'))
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+        return render_template('login.html', title='Sign In', loginform=loginform, signupform=signupform)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index')
+    return redirect(url_for('index'))
