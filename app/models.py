8e5753627f7f8b69c0d544ed2a86853b7b6ff037
Julian Lee <julian.lee@thecrimson.com>
2020-01-13 05:46:21 -0500

    Updated database with questions

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(UserMixin, db.Model):
     lastname = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    babyname = db.Column(db.String(64))
+    babybirth = db.Column(db.Date)
  
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -17,6 +19,31 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
     def __repr__(self):
         return '<User {}>'.format(self.username)    
+
+class RawQuestion(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.Text)
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    created_by = db.Column(db.Integer, db.ForeignKey(User.id))
+
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('modifiedquestion.id'), primary_key=True)
+)
+
+class ModifiedQuestion(RawQuestion): 
+    __tablename__ = 'modifiedquestion'
+    likes = db.Column(db.Integer)
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+        backref=db.backref('modifiedquestion', lazy=True))
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
